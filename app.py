4cb4b0b1b7853e9b6a8a511028285c4ba43cc88d
unknown <jbnscj@gmail.com>
2023-01-14 15:00:30 +0100

    finalised word cloud

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ if __name__ == "__main__":
     scatterplot1 = Scatterplot("Scatterplot 1", "sepal_length", "sepal_width", df)
     scatterplot2 = Scatterplot("Scatterplot 2", "petal_length", "petal_width", df)
     #scatterplot3 = Scatterplot("Scatterplot 3", "petal_length", "petal_width", df)
-    wordcloud = WordsCloud("wordcloud", df2)
+    wordcloud = WordsCloud("wordcloud", "Advertising of AirBnbs in selected area", df2)
 
     histogram = Histogram(
         "Distribution of number of Airbnbs owned by individual owners in selected area",
@@ -63,8 +63,8 @@ if __name__ == "__main__":
                         children=make_menu_layout(),
                     ),
                     histogram,
-                    scatterplot2,
                     wordcloud,
+                    scatterplot2,
                     horizontal_bar,
                 ],
             ),
@@ -92,13 +92,10 @@ if __name__ == "__main__":
     def update_scatter_2(selected_color, selected_data):
         return scatterplot2.update(selected_color, selected_data)
 
-    @app.callback(
-        Output(wordcloud.html_id, "src"),
-        Input(wordcloud.html_id, "id")
-    )
-    def update_wc(id):
+
+    def update_wc(neighbourhood):
         img = BytesIO()
-        wordcloud.update().save(img, format="PNG")
+        wordcloud.update(neighbourhood).save(img, format="PNG")
         return 'data:image/png;base64,{}'.format(base64.b64encode(img.getvalue()).decode())
 
 
@@ -110,16 +107,19 @@ if __name__ == "__main__":
             Output("zip_code_text", "value"),
             Output(histogram.html_id, "figure"),
             Output(horizontal_bar.html_id, "figure"),
+            Output(wordcloud.html_id, "src"),
         ],
         [
             Input("select_neigh", "value"),
             Input("zip_code_text", "value"),
             Input("local_switch", "on"),
         ],
-        [State("header_title", "children"), State(histogram.html_id, "figure")],
+        [State("header_title", "children"), State(histogram.html_id, "figure"),
+        State(horizontal_bar.html_id, "figure"),
+        State(wordcloud.html_id, "src")],
     )
     def update_neighbourhoods(
-        select_name, zip_code_text, local_switch, header_state, histogram_current
+        select_name, zip_code_text, local_switch, header_state, histogram_current, hb_current, wordcloud_current
     ):
         if zip_code_text is not None:
             if (not zip_code_text.isdigit()) or (len(zip_code_text) != 5):
@@ -128,6 +128,8 @@ if __name__ == "__main__":
                     "The value entered is not a valid zip code.",
                     zip_code_text,
                     histogram_current,
+                    hb_current,
+                    wordcloud_current
                 )
             else:
                 # Load data
@@ -145,6 +147,8 @@ if __name__ == "__main__":
                         "The value entered does not correspond to a New York neighbourhood.",
                         zip_code_text,
                         histogram_current,
+                        hb_current,
+                        wordcloud_current
                     )
                 else:
                     neighbourhood = df_filter[["neighbourhood"]].iloc[0][0]
@@ -154,6 +158,7 @@ if __name__ == "__main__":
                         "",
                         histogram.update(neighbourhood, local_switch),
                         horizontal_bar.update(None),
+                        update_wc(neighbourhood)
                     )
         elif select_name != "All":
             return (
@@ -162,6 +167,7 @@ if __name__ == "__main__":
                 None,
                 histogram.update(select_name, local_switch),
                 horizontal_bar.update(None),
+                update_wc(select_name)
             )
         else:
             return (
@@ -170,6 +176,7 @@ if __name__ == "__main__":
                 None,
                 histogram.update(None, local_switch),
                 horizontal_bar.update(None),
+                update_wc(None)
             )
 
     app.run_server(debug=False, dev_tools_ui=False)
