3b11b6d488f0dfc8ae2d2e0cbb79bd8449cdcaf1
olivier boeren <olivierboeren@live.nl>
2023-01-21 10:37:55 +0100

    readd colours

---
+++ jbi100_app/views/colors.py
@@ -0,0 +1,12 @@
+bg_colour = "#121212"
+card_colour = "#212121"
+txt_colour = "#f1f1f1"
+
+#
+marker_1 = "#0090e7"
+marker_2 = "#00d25b"
+marker_3 = "#8f5fe8"
+marker_4 = "#fc424a"
+marker_5 = "#ffab00"
+
+colour_gradient = "Viridis"
