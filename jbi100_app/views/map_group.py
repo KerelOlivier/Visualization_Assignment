6c8c4118d7c9d70513c8ce2bd5e76175f1b96bd3
olivier boeren <olivierboeren@live.nl>
2023-01-21 05:23:58 +0100

    fix: wrong initial map

--- jbi100_app/views/map_group.py
+++ jbi100_app/views/map_group.py
@@ -20,7 +20,7 @@ class MapGroup(html.Div):
             ],
         )
 
-  def update(self, map_mode='scatter', alarm_mode=None, loc_change=False, colours=[clrs.marker_2, clrs.marker_4], neighbourhood=None):
+  def update(self, map_mode='fire', alarm_mode=None, loc_change=False, colours=[clrs.marker_2, clrs.marker_4], neighbourhood=None):
     if map_mode == 'fire' or map_mode == 'co':
       self.html_id = self.scatter.html_id      
       self.fig = self.scatter.update(mode=map_mode, loc_change=loc_change, colours=colours, neighbourhood=neighbourhood)
