3c6ea385b27a0819a70107c3f90d42a3d5896bd6
olivier boeren <olivierboeren@live.nl>
2023-01-24 17:36:21 +0100

    fix: stupid colour gradient

--- jbi100_app/views/colors.py
+++ jbi100_app/views/colors.py
@@ -24,21 +24,21 @@ card_colour = "#212121"
 txt_colour = "#f1f1f1"
 line_colour = "#424242"
 
-marker_1 = "#548C2F"   # Green
+marker_1 = "#FFD449"   # Yellow
 marker_2 = "#A8D5E2"   # Light blue
 marker_3 = "#F9A620"   # Orange
-marker_4 = "#FFD449"   # Yellow
-marker_5 = "#A8D5E2"   # Dark green
+marker_4 = "#A8D5E2"   # Dark green
+marker_5 = "#548C2F"   # Green
 marker_off = "#a8a8a8" # Grey
 
-colour_gradient = "Viridis" 
+colour_gradient = "Greens" 
 
 # Word cloud colour functions
 def my_tf_color_func(dictionary):
     def my_tf_color_func_inner(
         word, random_state=None, **kwargs
     ):
-        return "hsl(90, 50%%, %d%%)" % (dictionary[word])
+        return "hsl(46, 100%%, %d%%)" % (dictionary[word])
 
     return my_tf_color_func_inner
 #colour function for word cloud in colourblind mode
@@ -46,7 +46,7 @@ def my_tf_color_func_cb(dictionary):
     def my_tf_color_func_inner(
         word, random_state=None, **kwargs
     ):
-        return "hsl(174, 37%%, %d%%)" % (dictionary[word])
+        return "hsl(47, 99%%, %d%%)" % (dictionary[word])
 
     return my_tf_color_func_inner
 
